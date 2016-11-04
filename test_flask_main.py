ebeb0b96e7a651c49288b08e4f1e481ea0eaa461
Holden Oullette <holdenoullette@gmail.com>
2016-11-04 15:27:18 -0700

    added nosetest suite

--- test_flask_main.py
+++ test_flask_main.py
@@ -2,63 +2,61 @@ from flask_main import *
 import arrow
 
 def test_database_add():
-  date = "2016-11-01"
-  memo = "RANDOM TEXT"
-  entry = database_entry(date, memo)
-  assert(entry["date"] == arrow.get(date).replace(tzinfo = tz.tzlocal()).isoformat())
-  assert(entry["text"] == memo)
+    date = "2016-11-01"
+    memo = "RANDOM TEXT"
+    entry = database_entry(date, memo)
+    assert(entry["date"] == arrow.get(date).replace(tzinfo = tz.tzlocal()).isoformat())
+    assert(entry["text"] == memo)
 
-  date = "2016-02-28"
-  memo = "RANDOM TEXT 2"
-  entry = database_entry(date, memo)
-  assert(entry["date"] == arrow.get(date).replace(tzinfo = tz.tzlocal()).isoformat())
-  assert(entry["text"] == memo)
+    date = "2016-02-28"
+    memo = "RANDOM TEXT 2"
+    entry = database_entry(date, memo)
+    assert(entry["date"] == arrow.get(date).replace(tzinfo = tz.tzlocal()).isoformat())
+    assert(entry["text"] == memo)
 
-  date = "2016-01-01"
-  memo = ""
-  entry = database_entry(date, memo)
-  assert(entry["date"] == arrow.get(date).replace(tzinfo = tz.tzlocal()).isoformat())
-  assert(entry["text"] == memo)
+    date = "2016-01-01"
+    memo = ""
+    entry = database_entry(date, memo)
+    assert(entry["date"] == arrow.get(date).replace(tzinfo = tz.tzlocal()).isoformat())
+    assert(entry["text"] == memo)
 
 def test_database_delete():
-  memos = get_memos()
+    date = "2016-01-01"
+    memo = ""
+    for i in range(0,10):
+        collection.insert(database_entry(date, memo))
+    memos = get_memos()
 
-  to_destroy_str = "1,"
-  to_destroy = to_destroy_str.strip(',').split(',')
-  destroyed = []
-  for index in to_destroy:
-    destroyed.append(sorted_memos[int(index)-1])
-  assert(destroy_helper(to_destroy_str) ==  destroyed)
+    checkboxes = "1,3"
+    test_delete = checkboxes.split(',')
+    deleted = []
+    for index in test_delete:
+        deleted.append(memos[int(index)])
+    assert(database_removal(checkboxes) ==  deleted)
 
-  #if butto clicked but no boxes checked
-  to_destroy_str = ""
-  destroyed = []
-  assert(destroy_helper(to_destroy_str) ==  destroyed)
+    checkboxes = "2,5,"
+    test_delete = checkboxes.split(',')
+    deleted = []
+    for index in test_delete:
+        if index == '':
+            break
+        deleted.append(memos[int(index)])
+    assert(database_removal(checkboxes) ==  deleted)
 
-  to_destroy_str = "1,5,"
-  to_destroy = to_destroy_str.strip(',').split(',')
-  destroyed = []
-  for index in to_destroy:
-    destroyed.append(sorted_memos[int(index)-1])
-  assert(destroy_helper(to_destroy_str) ==  destroyed)
+    checkboxes = ""
+    deleted = []
+    assert(database_removal(checkboxes) ==  deleted)
 
 
 def test_humanize():
-  """
-  test humanization of dates
-  """
-  now = arrow.now()
+    date = arrow.now().replace(days =- 5)
+    assert(humanize_arrow_date(date) == "5 days ago" )
 
-  date = now.replace(days =- 2)
-  assert(humanize_arrow_date(date) == "2 days ago" )
+    date = arrow.now().replace(days =- 1)
+    assert(humanize_arrow_date(date) == "Yesterday" )
 
-  date = now.replace(days =- 1)
-  assert(humanize_arrow_date(date) == "Yesterday" )
+    date = arrow.now().replace(days =- 0)
+    assert(humanize_arrow_date(date) == "Today" )
 
-  date = now.replace(days =- 0)
-  assert(humanize_arrow_date(date) == "Today" )
-
-  date = now.replace(days =+ 1)
-  assert(humanize_arrow_date(date) == "Tomorrow" )
-
-test_database_add()
+    date = arrow.now().replace(days =+ 1)
+    assert(humanize_arrow_date(date) == "Tomorrow" )
